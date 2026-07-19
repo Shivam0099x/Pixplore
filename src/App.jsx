a7cbed97383f41d6f4697e01bfc53d9827225455
Shivam Patel <shivampatel722490@gmail.com>
2026-07-20 02:37:53 +0530

    Implement SearchBar and Tabs components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { fetchPhotos,fetchVideos, fetchGifs, fetchStickers } from './api/mediaApi'
+import {setQuery} from './redux/Features/searchSlice'
+import SearchBar from './components/SearchBar'
+import Tabs from './components/Tabs'
+import ResultGrid from './components/ResultGrid'
 
 const App = () => {
   return (
     <div>
-      <button onClick={()=>fetchPhotos("cat")} className='px-2 py-2 bg-red-800 rounded-lg m-10'>Photos</button>
-      <button onClick={()=>fetchVideos("cat")} className='px-2 py-2 bg-red-800 rounded-lg m-10'>Videos</button>
-      <button onClick={()=>fetchGifs("cat")} className='px-2 py-2 bg-red-800 rounded-lg m-10'>Gifs</button>
-      <button onClick={()=>fetchStickers("cat")} className='px-2 py-2 bg-red-800 rounded-lg m-10'>Stickers</button>
+      <SearchBar/>
+      <Tabs/>
+      <ResultGrid/>
     </div>
   )
 }
