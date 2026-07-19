b80a7e939a64d3f08c65796cb8a70d8829c5cf8e
Shivam Patel <shivampatel722490@gmail.com>
2026-07-19 18:36:39 +0530

    search slice for media search functionality

---
+++ src/redux/Features/searchSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const searchSlice = createSlice({
+    name:'search',
+    initialState :{
+        query : '',
+        activeTab : 'photos',
+        result : [],
+        loading : false,
+        error: null
+    },
+    reducers:{
+       setQuery : (state,action)=>{
+          state.query = action.payload
+       },
+       setActiveTabs : (state,action)=>{
+          state.activeTab = action.payload
+       },
+       setResults : (state,action)=>{
+          state.result = action.payload
+          state.loading = false;
+       },
+       setLoading : (state,action)=>{
+          state.loading = true,
+          state.error = null
+       },
+       setErrors : (state,action)=>{
+          state.error = action.payload
+          state.loading = false
+       },
+       clearResults:(state)=>{
+        state.results = []
+       }
+    }
+})
+
+
+export const {setQuery, setErrors, setLoading, setResults, setActiveTabs, clearResults } = searchSlice.actions
+export default searchSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './Features/counterSlice'
+import searchReducers from './Features/searchSlice'
+
 
 export const store = configureStore({
   reducer: {
-    counter : counterReducer
+    search : searchReducers,
   },
 })
