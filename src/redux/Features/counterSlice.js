c06f98122f016d910075f9023a2f5b71a69d9b4f
Shivam Patel <shivampatel722490@gmail.com>
2026-07-19 10:49:14 +0530

    Initialize Project

---
+++ src/redux/Features/counterSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name:'counter',
+    initialState :{
+        value: null
+    },
+    reducers:{
+       
+    }
+})
+
+
+export const { } = counterSlice.actions
+export default counterSlice.reducer
