c17275dcc6c4a5b342c9966b86b810eb65b4ccad
Shivam Patel <shivampatel722490@gmail.com>
2026-07-20 12:46:56 +0530

    Add collection slice for managing media collection with local storage support

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import searchReducers from './Features/searchSlice'
+import collectionReducers from './Features/collectionSlice'
 
 
 export const store = configureStore({
   reducer: {
     search : searchReducers,
+    collection : collectionReducers
   },
 })
